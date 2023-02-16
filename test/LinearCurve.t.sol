cafcceb8cbdd4ac9cb13d1dfc7b81690988fde73
Ratimon <ratixoxo@gmail.com>
2023-02-16 18:34:15 +0700

    Test (./test): Adding Fuzzing test for getInstantaneousPrice()

--- test/LinearCurve.t.sol
+++ test/LinearCurve.t.sol
@@ -2,6 +2,8 @@
 pragma solidity =0.8.17;
 
 import {Test} from "@forge-std/Test.sol";
+import {StdUtils} from "@forge-std/StdUtils.sol";
+
 import { Assertions as PRBMathAssertions } from "@prb-math/test/Assertions.sol";
 import { UD60x18, ud } from "@prb-math/UD60x18.sol";
 // import {PRBTest } from "@prb-test/PRBTest.sol";
@@ -9,28 +11,35 @@ import { UD60x18, ud } from "@prb-math/UD60x18.sol";
 
 import {MockLinearCurve} from "@main/mocks/MockLinearCurve.sol";
 
-contract TestLinearCurve is PRBMathAssertions {
-
-// contract TestLinearCurve is PRBTest, PRBMathAssertions {
+contract TestLinearCurve is StdUtils,PRBMathAssertions {
 
     uint256 immutable SLOPE = 1.5e18;
     uint256 immutable INTITIAL_PRICE = 30e18;
 
     MockLinearCurve LinearCurveContract;
 
-
     function setUp() public {
         LinearCurveContract = new MockLinearCurve(SLOPE, INTITIAL_PRICE);
     }
 
     function test_getInstantaneousPrice() external {
-
         UD60x18 tokenAmountIn = ud(20e18);
 
         UD60x18 actualPrice = LinearCurveContract.getInstantaneousPrice(tokenAmountIn);
         UD60x18 expectedPrice = ud(60e18);
-        
-        //20*1.5+30 = 60
+        // 20*1.5 + 30 = 60
+        assertEq(actualPrice, expectedPrice);
+    }
+
+    function testFuzz_getInstantaneousPrice(uint256 tokenSupply) external {
+
+        tokenSupply = bound( tokenSupply, 0.5e18, 200e18);
+
+        UD60x18 tokenAmountIn = ud(tokenSupply);
+
+        UD60x18 actualPrice = LinearCurveContract.getInstantaneousPrice(tokenAmountIn);
+        UD60x18 expectedPrice = tokenAmountIn.mul(ud(SLOPE)).add(ud(INTITIAL_PRICE));
+
         assertEq(actualPrice, expectedPrice);
 
     }
